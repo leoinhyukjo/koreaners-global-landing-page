4349b2d7661f41f89df9cacb370b733cb7f5b90f
leo <leo@leo-MacBook-Air-M4.local>
2026-02-05 18:12:05 +0900

    chore: remove Microsoft Clarity script only

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,16 +66,6 @@ export default function RootLayout({
             gtag('config', 'G-Z1TDSYTVVR');
           `}
         </Script>
-        {/* MS Clarity */}
-        <Script id="clarity" strategy="afterInteractive">
-          {`
-            (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "vcdon3oki9");
-          `}
-        </Script>
         {/* Meta Pixel */}
         <Script id="fb-pixel" strategy="afterInteractive">
           {`
